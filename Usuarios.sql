6dcde26b92f9d5aac8331f16e04b791f147a5031
Paolo <paolo_1433@outlook.com>
2024-11-06 15:51:26 -0600

    Aqui se agrego la primera modificacion con la tabla registro y equipo

--- Usuarios.sql
+++ Usuarios.sql
@@ -7,3 +7,18 @@ CREATE TABLE `Usuarios`(
     `Actividad` VARCHAR(255) NOT NULL,  -- 
     `Cuatrimestre` TINYINT UNSIGNED NULL  
 ); 
+CREATE TABLE `Registro`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `Id_Computadora` INT NOT NULL,
+    `Id_Usuarios` INT NOT NULL,
+    `Tiempo` TIMESTAMP NOT NULL
+);
+CREATE TABLE `Equipo`(
+    `id_Computadora` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `modelo` VARCHAR(255) NOT NULL,
+    `Características` LONGTEXT NOT NULL
+);
+ALTER TABLE
+    `Registro` ADD CONSTRAINT `registro_id_computadora_foreign` FOREIGN KEY(`Id_Computadora`) REFERENCES `Equipo`(`id_Computadora`);
+ALTER TABLE
+    `Registro` ADD CONSTRAINT `registro_id_usuarios_foreign` FOREIGN KEY(`Id_Usuarios`) REFERENCES `Usuarios`(`Id_Usuarios`);
