9c7b494daf231a7fabe559413ab1fcdbb0969d3d
Paolo <paolo_1433@outlook.com>
2025-03-03 20:59:15 -0600

    Se agrega la actualizacion de la segunda pagina

---
+++ sesion/segundapag.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../estilos/base1.css">
+    <title>Segunda página</title>
+</head>
+<body>
+     <!-- LOGO  -->
+     <div class="container">
+        <img src="https://lh3.googleusercontent.com/3Kh8yi_6rA8bEWMHZpGTyMreuSipv0m-KiZo7HoaOkIpVQc2ucyqQBups3Ye7VOTQlPjH7THp3E60B6ejnFYdnYzaLHysv_k=s580" alt="Logo" style="width: 160px; height: auto; margin-bottom: 9px;">
+      <!-- Registro  -->
+         <p class="greeting">¡Bienvenido/a!</p>
+        <p>Corroborá el ingreso de datos.</p>
+        <p class="greeting">ID de usuario</p> 
+        <input type="text"required>
+        <div class="show-container">
+            <label>Nombre</label><br>
+
+            <label>Actividad</label><br>
+            
+            <label>Área</label><br>
+            
+            <label>Cuatrimestre</label><br>
+            
+            <label>Horario de la actividad</label><br>
+            
+            <button>Iniciar actividad</button>
+        </div>
+       </div>
+</body>
+</html>
